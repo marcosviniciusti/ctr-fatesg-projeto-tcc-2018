6ca7b070a8f77ab60c0758f2f7db474502969e32
dudmacedo <dudmacedo@gmail.com>
2018-06-11 20:20:26 -0300

    Recriado projeto para o NodeMCU

---
+++ CTRNodeMCU/src/main.cpp
@@ -0,0 +1,24 @@
+/*________________________FRAMEWORK________________________*/
+#include <Arduino.h>
+
+/*______________________INFRAVERMELHO______________________*/
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+/*__________________________JSON___________________________*/
+#include <ArduinoJson.h>
+
+/*__________________________WIFI___________________________*/
+#include <ESP8266WiFi.h>
+
+/*__________________________MQTT___________________________*/
+#include <PubSubClient.h>
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
