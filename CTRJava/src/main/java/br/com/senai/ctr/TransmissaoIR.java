bac0a20f86be4b384ba9feb5c253ab0c52f48c59
dudmacedo <dudmacedo@gmail.com>
2018-06-23 12:50:19 -0300

    Refatoração de Código

--- CTRJava/src/main/java/br/com/senai/ctr/TransmissaoIR.java
+++ CTRJava/src/main/java/br/com/senai/ctr/TransmissaoIR.java
@@ -3,19 +3,9 @@ package br.com.senai.ctr;
 import java.util.List;
 
 public class TransmissaoIR {
-    private String idTransmissao;
     private String idEmissor;
     private String type;
     private List<Integer> rawData;
-
-    public String getIdTransmissao() {
-        return idTransmissao;
-    }
-
-    public TransmissaoIR setIdTransmissao(String idTransmissao) {
-        this.idTransmissao = idTransmissao;
-        return this;
-    }
 
     public String getIdEmissor() {
         return idEmissor;
